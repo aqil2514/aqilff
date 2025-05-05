24d61a2720dce5e2f3a5bdb9b5187beec716e594
Muhamad Aqil Maulana <muhamadaqil383@gmail.com>
2025-05-05 10:57:26 +0700

    [Landing Page] : Fix UI

--- landing-page/src/components/Routes/Products/ProductList/index.tsx
+++ landing-page/src/components/Routes/Products/ProductList/index.tsx
@@ -8,8 +8,8 @@ export default function ProductList() {
   return (
     <div className="h-[80vh] overflow-y-auto px-2 py-4">
       {isAvailable ? (
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-6">
-          {filteredProducts.map((product) => (
+        <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-4 sm:gap-6">
+        {filteredProducts.map((product) => (
             <ProductCard key={product.id} product={product} />
           ))}
         </div>
