5b9477fdf26bdf0b43ae7b6aedabfed29c415060
Muhamad Aqil Maulana <muhamadaqil383@gmail.com>
2025-05-05 10:47:37 +0700

    Fix Bug

--- landing-page/src/components/Routes/Products/index.tsx
+++ landing-page/src/components/Routes/Products/index.tsx
@@ -5,13 +5,24 @@ import ProductsProvider from "@/components/Providers/ProductsProvider";
 import ProductList from "./ProductList";
 import MainWrapper from "@/components/Reuseable/main-wrapper";
 import ProductFilter from "./ProductFilter";
+import { Suspense } from "react";
 
-export default function ProductsRoute({ products, category }: { products: Product[], category:Category[] }) {
+export default function ProductsRoute({
+  products,
+  category,
+}: {
+  products: Product[];
+  category: Category[];
+}) {
   return (
     <ProductsProvider products={products} category={category}>
       <MainWrapper background="gradientRedToOrange">
-        <ProductFilter />
-        <ProductList />
+        <Suspense fallback={<div>Loading Filter...</div>}>
+          <ProductFilter />
+        </Suspense>
+        <Suspense fallback={<div>Loading Products...</div>}>
+          <ProductList />
+        </Suspense>
       </MainWrapper>
     </ProductsProvider>
   );
